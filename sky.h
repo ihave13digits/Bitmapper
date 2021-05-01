beea4329116b1707ae6caafb627a55cd0cc65173
ihave13digits <ihave13digits@protonmail.com>
2021-05-01 00:27:14 -0400

    updated core

--- sky.h
+++ sky.h
@@ -8,6 +8,7 @@ public:
     float hue = -0.5;
     float time = 1.0;
     float move = 1.0;
+    float speed = 0.01;
 
     int width = 0;
     int height = 0;
@@ -240,7 +241,7 @@ public:
 
     void Update(float delta)
     {
-        UpdateTime(delta*2.0);
+        UpdateTime(delta*speed);
         UpdateBodies();
         UpdateColor();
     }
