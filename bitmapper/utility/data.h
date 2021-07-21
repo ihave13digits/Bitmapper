7020862e4446ce851a9ca50273aba5ae03ae004e
ihave13digits <ihave13digits@protonmail.com>
2021-07-20 21:45:32 -0400

    updated core

--- bitmapper/utility/data.h
+++ bitmapper/utility/data.h
@@ -1,23 +1,73 @@
 namespace dataTool
 {
     //std::string path_ = "/bitmapper/";
-    std::string path_player = "/data/player/";
-    std::string path_world = "/data/world/";
+    std::string path_data = "/data/";
+    std::string path_player = path_data+"player/";
+    std::string path_world = path_data+"world/";
     std::string path_tile = "/bitmapper/environment/tile/";
 
     //std::string file_ = "";
-    std::string file_tile = "tile.data";
+    std::string file_tile =     "tile.data";
+    std::string file_settings = "settings.data";
 
     std::string GetPath(std::string extension)
     {
         return os::GetCWD() + extension;
     }
 
+    void SaveSettings()
+    {
+        std::fstream data_file;
+        std::string _dir = os::GetCWD() + path_data + file_settings;
+        data_file.open(_dir);
+
+        if (data_file.is_open())
+        {
+            std::cout <<"Saving Settings" <<std::endl;
+            data_file <<core::selected_hotbar <<std::endl;
+            data_file <<core::selected_tile <<std::endl;
+            data_file <<core::selected_wand <<std::endl;
+            data_file <<core::width <<std::endl;
+            data_file <<core::height <<std::endl;
+            data_file <<core::resolution <<std::endl;
+            data_file <<core::grid_subdivision <<std::endl;
+            data_file.close();
+        }
+        else
+        {
+            std::ofstream new_file (_dir);
+            SaveSettings();
+        }
+    }
+    void LoadSettings()
+    {
+        std::fstream data_file;
+        std::string _dir = os::GetCWD() + path_data + file_settings;
+        data_file.open(_dir);
+        
+        if (data_file.is_open())
+        {
+            std::cout <<"Loading Settings" <<std::endl;
+            data_file.close();
+        }
+    }
+
     void GenerateDirectoryTree()
     {
-        std::string _dir = os::GetCWD() + "/data";
-        std::string _cmd = "mkdir " + _dir;
-        const char* mkdir_cmd = _cmd.c_str();
-        system(mkdir_cmd);
+        std::fstream data_file;
+        std::string _dir = os::GetCWD() + path_data + file_settings;
+        data_file.open(_dir);
+
+        if (data_file.is_open())
+        {
+            std::cout <<"Directory Tree Is Valid, Starting Bitmapper" <<std::endl;
+            data_file.close();
+        }
+        else
+        {
+            std::cout <<"Generating Directory Tree" <<std::endl;
+            os::MakeDirectory(path_data);
+            SaveSettings();
+        }
     }
 }
--- bitmapper/utility/platform.h
+++ bitmapper/utility/platform.h
@@ -12,11 +12,19 @@
 
 namespace os
 {
-std::string GetCWD()
+    std::string GetCWD()
+        {
+            char buff[FILENAME_MAX];
+            GetCurrentDir(buff, FILENAME_MAX);
+            std::string _dir(buff);
+            return _dir;
+        }
+
+    void MakeDirectory(std::string path_data)
     {
-        char buff[FILENAME_MAX];
-        GetCurrentDir(buff, FILENAME_MAX);
-        std::string _dir(buff);
-        return _dir;
+        std::string _dir = os::GetCWD() + path_data;
+        std::string _cmd = "mkdir " + _dir;
+        const char* mkdir_cmd = _cmd.c_str();
+        system(mkdir_cmd);
     }
 }
