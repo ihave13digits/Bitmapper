c36399b857e798730d91a309c6e8ab071e0fb7d0
ihave13digits <ihave13digits@protonmail.com>
2021-07-28 22:31:53 -0400

    updated core

--- bitmapper/utility/utility.h
+++ bitmapper/utility/utility.h
@@ -7,6 +7,7 @@ namespace textTool
         if (
             c=="-" || c=="." || c=="1" || c=="2" || c=="3" || c=="4" || c=="5" || c=="6" || c=="7" || c=="8" || c=="9" || c=="0"
             ) { can_do = true; }
+        return can_do;
     }
 
     bool IsLetter(std::string c)
